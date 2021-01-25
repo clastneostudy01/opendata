a4d96561dc8c6a028a115abb66956b2fe1d80998
clastneostudy01 <77958150+clastneostudy01@users.noreply.github.com>
2021-01-25 17:11:44 +0900

    Add files via upload

---
+++ src/test/java/com/example/myworkspace/OpendataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpendataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
